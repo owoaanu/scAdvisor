fa38d9a485f89e127aefceda373855b3a6f3e187
samuel-owoaanu <sam.adebayoadesina@gmail.com>
2024-12-11 14:05:52 +0000

    online change in setting and urls files

--- smartapp/urls.py
+++ smartapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns =  [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
@@ -11,5 +12,6 @@ urlpatterns =  [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
-    
+    # path('git-update', views.git_update, name='git-update'),
+
 ]
--- smartproject/settings.py
+++ smartproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$kbq5mmqmt#ug@qca5uu1n%vkdnh(ru%v55q@rvvg&bk$u_3n4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
