7052681a47dab00c83b7d9e1eddf0c10320fed79
Ayodeji Samuel Makinde <makinde6@gmail.com>
2025-06-05 14:46:05 +0300

    callbak

--- smartapp/urls.py
+++ smartapp/urls.py
@@ -19,10 +19,10 @@ urlpatterns =  [
     
     path('ems-callback/', views.ems_data_callback, name='ems_callback'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    
+
     path('ems-image-callback/', views.ems_image_callback, name='ems_image_callback'),
-     
-    ]
+
+]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
